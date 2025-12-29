8cd9c32d0dbdd83e7cc19094882eaacc599a0671
KhushiGupta <kg8616549@gmail.com>
2025-12-29 06:50:46 +0000

    variables,datatype

---
+++ basics_01/datatype.js
@@ -0,0 +1,5 @@
+"use strict";
+
+//alert("20 degree temp")
+
+console.log(typeof null);
---
+++ basics_01/var.js
@@ -0,0 +1,16 @@
+const personId = 1
+let personEmail = "khushi1@gmail.com"
+var personPassword = "123"
+personCity = "Saharanpur"
+
+// console.log(personId);
+// console.log(personEmail);
+
+//console.log(personCity);
+
+// personId = 5
+personEmail = "kg1@gmail.com"
+
+//console.log(personEmail);
+
+console.table([personId, personEmail, personPassword, personCity])
