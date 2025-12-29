474ebc62e9634a9ef6d9e2cb922388ec0a58ea08
KhushiGupta <kg8616549@gmail.com>
2025-12-29 07:13:19 +0000

    typeconversion

---
+++ basics_01/typeconv.js
@@ -0,0 +1,22 @@
+let score= 63
+let name = "khushi"
+
+// console.log(typeof score);
+
+// console.log(typeof null);
+// console.log(typeof true);
+// console.log(typeof name);
+
+// name =1
+
+// console.log(typeof name);
+
+// let isPunchIn = 1
+// let booleanIsPunchIn = Boolean(isPunchIn)
+
+// console.log(booleanIsPunchIn)
+
+let stringNum = String(score)
+console.log(typeof stringNum)
+console.log(score + 2)
+console.log(stringNum + 2);
